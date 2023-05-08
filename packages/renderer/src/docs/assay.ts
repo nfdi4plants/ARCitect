9033c32c5fffd3f3e66d736d56c0546d4abaf3ab
brilator <dominik.brilhaus@hhu.de>
2023-05-08 17:51:36 +0200

    test add some text to assay help

--- packages/renderer/src/docs/assay.ts
+++ packages/renderer/src/docs/assay.ts
@@ -1,5 +1,11 @@
 export default `
 # Assay
 
-Assay help text
+In this area you can organize the assays of your ARC.
+
+- Create a new assay by clicking "Add Assay"
+- Update the assay metadata by clicking your assay
+
+For more details, check out the DataPLANT knowledge base https://nfdi4plants.org/nfdi4plants.knowledgebase/docs/implementation/AnnotatedResearchContext.html
+
 `;
