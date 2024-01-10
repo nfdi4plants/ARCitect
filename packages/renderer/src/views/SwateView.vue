5b135e9d3bb6aa89dd1042a6f5dd0184f2bc85e4
Kevin F <Freymaurer@gmx.de>
2024-01-10 15:42:43 +0100

    Improve Swate communication logic

--- packages/renderer/src/views/SwateView.vue
+++ packages/renderer/src/views/SwateView.vue
@@ -4,15 +4,51 @@ import { reactive, onMounted, watch, ref } from 'vue';
 
 let iframe: any | HTMLElement = ref({})
 
-const SwateAPI = {
+enum Msg {
+  InitResponse,
+  Error
+}
+
+interface Message {
+  swate: boolean;
+  api: string;
+  data: any;
+}
+
+type MessageHandler = (data: any) => void;
+
+interface SwateAPI {
+  handleEvent: (e: MessageEvent) => void;
+  [key: string]: MessageHandler | ((msg: string) => void);
+}
+
+const send = (msg: Msg, data: any = null): void => {
+  const toSwate = (data: any): void => {
+    const methodName = Msg[msg];
+    const content: Message = {swate: true, api: methodName, data: data }
+    iframe.value.contentWindow?.postMessage(content, '*');
+  };
+  switch (msg) {
+    case Msg.InitResponse:
+      toSwate("Hello from ARCitect!");
+      break;
+    case Msg.Error:
+      toSwate(data);
+      break;
+    // Add more cases as needed
+  }
+};
+
+const SwateAPI : SwateAPI = {
   handleEvent: (e: MessageEvent) => {
     if (e.data.swate) {
-      SwateAPI[e.data.api](e.data.data);
+      const apiHandler = SwateAPI[e.data.api];
+      apiHandler(e.data.data);
     }
   },
-  init: () => {
-    console.log("swate/init")
-    iframe?.contentWindow?.postMessage({swate: true, api: "init", data: {name: "Kevin"}}, '*')
+  Init: (msg: string) => {
+    console.log(msg)
+    send(Msg.InitResponse)
   }
 }
 
@@ -20,6 +56,13 @@ onMounted(() => {
   window.addEventListener ("message", SwateAPI.handleEvent);
   iframe.value.setAttribute("src","http://localhost:8080")
 })
+
+/* 
+* This should be abstracted to a robust system for sending messages always in the same pattern 
+*/
+function initResponse() {
+  iframe.value.contentWindow?.postMessage({swate: true, api: "InitResponse", data: "Hello from ARCitect"}, '*')
+}
 
 </script>
 
