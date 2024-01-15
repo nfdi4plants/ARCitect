13d7919f9a3b620d6061181672163f4a0ccaf5cf
Dominik Brilhaus <brilhaus@nfdi4plants.org>
2024-01-15 13:29:08 +0100

    add tabs to main help

--- packages/renderer/src/docs/home.vue
+++ packages/renderer/src/docs/home.vue
@@ -1,12 +1,17 @@
-<script>
+<!-- 
+TODO: fix logo import 
+-->
+
+<script lang="ts">
 import { ref } from 'vue'
 
-import logoURL from '/assets/dpLogo2_w.png';
+import logoURL from '/assets/dpLogo2_w.png';  
 
 export default {
   setup() {
 
     return {
+      tab: ref('Sidebar'),
       selectedInv: ref('YourARC'),
       selectedStudy: ref('YourStudy'),
       selectedAssay: ref('YourAssay'),
@@ -85,264 +90,282 @@ export default {
 
 <template>
   <div class="q-pa-sm">
+
     <h5>Help</h5>
 
-    <div class="text-h6 text-weight-bold">Sidebar</div>
     <div class="q-pa-sm text-body2 text-justify">
-      Navigate the ARCitect via the sidebar on the left.
+      The ARCitect is divided into three panels: sidebar, file tree and main menu. 
     </div>
+    
+    <q-tabs v-model="tab" dense class="text-grey" active-color="primary" indicator-color="primary" align="justify"
+      narrow-indicator>
+      <q-tab name="sidebar" label="Sidebar" />
+      <q-tab name="filetree" label="File Tree" />
+    </q-tabs>
 
-    <q-list class="text-justify">
-      <q-item>
+    <q-tab-panels v-model="tab" animated>
+      <q-tab-panel name="sidebar">
 
-        <q-item class='bg-primary text-white' style="padding-top:1em;padding-bottom:1em;">
-          <q-item-section avatar>
-            <q-icon size="2.5rem" style="margin: 0 -0.20em;" :name="'img:' + logoURL"></q-icon>
-          </q-item-section>
-          <q-item-section style="margin:0.6em 0 0 -1.2em">
-            <q-item-label><b style="font-size:2em">ARC</b><span style="font-size:1.2em">itect</span></q-item-label>
-          </q-item-section>
-        </q-item>
+        <div class="q-pa-sm text-body2 text-justify">
+          Navigate the ARCitect via the sidebar on the left.
+        </div>
 
-        <q-separator inset />
+        <q-list class="text-justify bg-grey-2">
+          <q-item>
 
-        <q-item-section>
-          Return to home view
-        </q-item-section>
-      </q-item>
+            <q-item class='bg-primary text-white' style="padding-top:1em;padding-bottom:1em;">
+              <q-item-section avatar>
+                <q-icon size="2.5rem" style="margin: 0 -0.20em;" :name="'img:/assets/dpLogo2_w.png'"></q-icon>
+                <!-- <q-icon size="2.5rem" style="margin: 0 -0.20em;" :name="'img:' + logoURL"></q-icon> -->
+              </q-item-section>
+              <q-item-section style="margin:0.6em 0 0 -1.2em">
+                <q-item-label><b style="font-size:2em">ARC</b><span style="font-size:1.2em">itect</span></q-item-label>
+              </q-item-section>
+            </q-item>
 
-      <q-separator inset />
+            <q-separator inset />
 
-      <q-item>
-        <q-item-section avatar>
-          <q-icon color="grey-7" name="exit_to_app" />
-        </q-item-section>
-        <q-item-section style="margin-left: -1.2em;">Login</q-item-section>
-        <q-item-section>
-          Login to the DataHUB
-        </q-item-section>
-      </q-item>
+            <q-item-section>
+              Return to home view
+            </q-item-section>
+          </q-item>
 
-      <q-item>
-        <q-item-section avatar>
-          <q-icon color="grey-7" name="note_add" />
-        </q-item-section>
-        <q-item-section style="margin-left: -1.2em;">
-          New ARC
-        </q-item-section>
-        <q-item-section>
-          Create a new ARC on your computer
-        </q-item-section>
-      </q-item>
+          <q-separator inset />
 
-      <q-item>
-        <q-item-section avatar>
-          <q-icon color="grey-7" name="file_open" />
-        </q-item-section>
-        <q-item-section style="margin-left: -1.2em;">
-          Open ARC
-        </q-item-section>
-        <q-item-section>
-          Open an ARC existing from your computer
-        </q-item-section>
-      </q-item>
+          <q-item>
+            <q-item-section avatar>
+              <q-icon color="grey-7" name="exit_to_app" />
+            </q-item-section>
+            <q-item-section style="margin-left: -1.2em;">Login</q-item-section>
+            <q-item-section>
+              Login to the DataHUB
+            </q-item-section>
+          </q-item>
 
-      <q-item>
-        <q-item-section avatar>
-          <q-icon color="grey-7" name="cloud_download" />
-        </q-item-section>
-        <q-item-section style="margin-left: -1.2em;">
-          Download ARC
-        </q-item-section>
-        <q-item-section>
-          Download an ARC from the DataHUB
-        </q-item-section>
-      </q-item>
+          <q-item>
+            <q-item-section avatar>
+              <q-icon color="grey-7" name="note_add" />
+            </q-item-section>
+            <q-item-section style="margin-left: -1.2em;">
+              New ARC
+            </q-item-section>
+            <q-item-section>
+              Create a new ARC on your computer
+            </q-item-section>
+          </q-item>
 
-      <q-separator inset />
+          <q-item>
+            <q-item-section avatar>
+              <q-icon color="grey-7" name="file_open" />
+            </q-item-section>
+            <q-item-section style="margin-left: -1.2em;">
+              Open ARC
+            </q-item-section>
+            <q-item-section>
+              Open an ARC existing from your computer
+            </q-item-section>
+          </q-item>
 
-      <q-item>
-        <q-item-section avatar>
-          <q-icon color="grey-7" name="save" />
-        </q-item-section>
-        <q-item-section style="margin-left: -1.2em;">
-          Save ARC
-        </q-item-section>
-        <q-item-section>
-          <!-- TODO -->
-          Save the ARC locally
-        </q-item-section>
-      </q-item>
+          <q-item>
+            <q-item-section avatar>
+              <q-icon color="grey-7" name="cloud_download" />
+            </q-item-section>
+            <q-item-section style="margin-left: -1.2em;">
+              Download ARC
+            </q-item-section>
+            <q-item-section>
+              Download an ARC from the DataHUB
+            </q-item-section>
+          </q-item>
 
-      <q-item>
-        <q-item-section avatar>
-          <q-icon color="grey-7" name="refresh" />
-        </q-item-section>
-        <q-item-section style="margin-left: -1.2em;">
-          Refresh ARC
-        </q-item-section>
-        <q-item-section>
-          Refresh the open ARC (Reopens the latest state stored on your computer)
-        </q-item-section>
-      </q-item>
+          <q-separator inset />
 
-      <q-item>
-        <q-item-section avatar>
-          <q-icon color="grey-7" name="mediation" />
-        </q-item-section>
-        <q-item-section style="margin-left: -1.2em;">
-          Versioning
-        </q-item-section>
-        <q-item-section>
-          Track and communicate changes of your ARC
-        </q-item-section>
-      </q-item>
+          <q-item>
+            <q-item-section avatar>
+              <q-icon color="grey-7" name="save" />
+            </q-item-section>
+            <q-item-section style="margin-left: -1.2em;">
+              Save ARC
+            </q-item-section>
+            <q-item-section>
+              <!-- TODO -->
+              Save the ARC locally
+            </q-item-section>
+          </q-item>
 
-      <q-item>
-        <q-item-section avatar>
-          <q-icon color="grey-7" name="folder_open" />
-        </q-item-section>
-        <q-item-section style="margin-left: -1.2em;">
-          Explorer
-        </q-item-section>
-        <q-item-section>
-          Open the ARC in the explorer on your computer
-        </q-item-section>
-      </q-item>
+          <q-item>
+            <q-item-section avatar>
+              <q-icon color="grey-7" name="refresh" />
+            </q-item-section>
+            <q-item-section style="margin-left: -1.2em;">
+              Refresh ARC
+            </q-item-section>
+            <q-item-section>
+              Refresh the open ARC (Reopens the latest state stored on your computer)
+            </q-item-section>
+          </q-item>
 
-      <q-separator inset />
+          <q-item>
+            <q-item-section avatar>
+              <q-icon color="grey-7" name="mediation" />
+            </q-item-section>
+            <q-item-section style="margin-left: -1.2em;">
+              Versioning
+            </q-item-section>
+            <q-item-section>
+              Track and communicate changes of your ARC
+            </q-item-section>
+          </q-item>
 
-      <q-item>
-        <q-item-section avatar>
-          <q-icon color="grey-7" name="help" />
-        </q-item-section>
-        <q-item-section style="margin-left: -1.2em;">
-          Toggle Help
-        </q-item-section>
-        <q-item-section>
-          Show or hide the help menu
-        </q-item-section>
-      </q-item>
+          <q-item>
+            <q-item-section avatar>
+              <q-icon color="grey-7" name="folder_open" />
+            </q-item-section>
+            <q-item-section style="margin-left: -1.2em;">
+              Explorer
+            </q-item-section>
+            <q-item-section>
+              Open the ARC in the explorer on your computer
+            </q-item-section>
+          </q-item>
 
-      <q-item>
-        <q-item-section avatar>
-          <q-icon color="grey-7" name="chevron_left" />
-        </q-item-section>
-        <q-item-section style="margin-left: -1.2em;">
-          Minimize Sidebar
-        </q-item-section>
-        <q-item-section>
-          Minimize or maximize the sidebar
-        </q-item-section>
-      </q-item>
+          <q-separator inset />
 
-    </q-list>
+          <q-item>
+            <q-item-section avatar>
+              <q-icon color="grey-7" name="help" />
+            </q-item-section>
+            <q-item-section style="margin-left: -1.2em;">
+              Toggle Help
+            </q-item-section>
+            <q-item-section>
+              Show or hide the help menu
+            </q-item-section>
+          </q-item>
+
+          <q-item>
+            <q-item-section avatar>
+              <q-icon color="grey-7" name="chevron_left" />
+            </q-item-section>
+            <q-item-section style="margin-left: -1.2em;">
+              Minimize Sidebar
+            </q-item-section>
+            <q-item-section>
+              Minimize or maximize the sidebar
+            </q-item-section>
+          </q-item>
+
+        </q-list>
+
+
+      </q-tab-panel>
+
+      <q-tab-panel name="filetree">
+
+        <div class="q-pa-sm text-body2 text-justify">
+          Access and edit the files in your ARC via the file tree.
+        </div>
+
+        <q-list class="q-pa-md q-gutter-sm">
+
+          <q-item>
+            <q-item-section>
+              The root of your ARC by default contains the folders: studies - assays - workflows - runs
+            </q-item-section>
+          </q-item>          
+
+          <q-tree :nodes="ARCtreeInv" node-key="label" dense default-expand-all />
+
+          <q-separator inset />
+
+          <div class="text-subtitle1 text-weight-bold">Investigation</div>
+
+          <q-item>
+            <q-item-section>
+              Click on your ARC's name to open the "Investigation" menu
+            </q-item-section>
+          </q-item>
+
+
+          <q-tree :nodes="ARCtreeInv" node-key="label" dense selected-color='white' v-model:selected="selectedInv"
+            default-expand-all />
+
+          <q-separator inset />
+
+          <div class="text-subtitle1 text-weight-bold">Studies</div>
+
+          <q-item>
+            <q-item-section>
+              Unfold studies and click "Add Study" to add a new study to your ARC
+            </q-item-section>
+          </q-item>
+
+          <q-tree :nodes="addStudies" node-key="label" dense default-expand-all />
+
+          <q-separator inset />
+
+          <q-item>
+            <q-item-section>
+              Click on your study's name to open the "Study" menu
+            </q-item-section>
+          </q-item>
+
+          <q-tree :nodes="ARCtreeStudy" node-key="label" dense selected-color='white' v-model:selected="selectedStudy"
+            default-expand-all />
+
+          <q-separator inset />
+
+          <div class="text-subtitle1 text-weight-bold">Assays</div>
+
+          <q-item>
+            <q-item-section>
+              Unfold assays and click "Add Assay" to add a new assay to your ARC
+            </q-item-section>
+          </q-item>
+
+
+          <q-tree :nodes="addAssays" node-key="label" dense default-expand-all />
+
+          <q-separator inset />
+
+          <q-item>
+            <q-item-section>
+              Click on your assay's name to open the "Assay" menu
+            </q-item-section>
+          </q-item>
+
+
+
+          <q-tree :nodes="ARCtreeAssay" node-key="label" dense selected-color='white' v-model:selected="selectedAssay"
+            default-expand-all />
+
+
+          <q-separator inset />
+
+          <div class="text-subtitle1 text-weight-bold">Protocols</div>
+
+          <q-item>
+            <q-item-section>
+              Protocols can be added to individual studies or assays, respectively.
+            </q-item-section>
+          </q-item>
+
+          <q-tree :nodes="addProtocols" node-key="label" dense default-expand-all />
+
+          <q-separator inset />
+
+          <div class="text-subtitle1 text-weight-bold">Datasets</div>
+
+          <q-item>
+            <q-item-section>
+              Dataset files or folders can be imported to individual assays.
+            </q-item-section>
+          </q-item>
+
+          <q-tree :nodes="importDatasets" node-key="label" dense default-expand-all />
+
+        </q-list>
+      </q-tab-panel>
+    </q-tab-panels>
   </div>
-
-  <q-separator size=".5em" />
-
-  <div class="text-h6 text-weight-bold">File tree</div>
-  <div class="q-pa-sm text-body2 text-justify">
-    Access and edit the files in your ARC via the file tree.
-  </div>
-
-  <q-list class="q-pa-md q-gutter-sm">
-
-    <q-item>
-      <q-item-section>
-        The root of your ARC by default contains the folders: studies - assays - workflows - runs
-      </q-item-section>
-    </q-item>
-
-    <q-separator inset />
-
-    <q-tree :nodes="ARCtreeInv" node-key="label" dense default-expand-all />
-
-    <div class="text-subtitle1 text-weight-bold">Investigation</div>
-
-    <q-item>
-      <q-item-section>
-        Click on your ARC's name to open the "Investigation" menu
-      </q-item-section>
-    </q-item>
-
-
-    <q-tree :nodes="ARCtreeInv" node-key="label" dense selected-color='white' v-model:selected="selectedInv"
-      default-expand-all />
-
-    <q-separator inset />
-
-    <div class="text-subtitle1 text-weight-bold">Studies</div>
-
-    <q-item>
-      <q-item-section>
-        Unfold studies and click "Add Study" to add a new study to your ARC
-      </q-item-section>
-    </q-item>
-
-    <q-tree :nodes="addStudies" node-key="label" dense default-expand-all />
-
-    <q-separator inset />
-
-    <q-item>
-      <q-item-section>
-        Click on your study's name to open the "Study" menu
-      </q-item-section>
-    </q-item>
-
-    <q-tree :nodes="ARCtreeStudy" node-key="label" dense selected-color='white' v-model:selected="selectedStudy"
-      default-expand-all />
-
-    <q-separator inset />
-
-    <div class="text-subtitle1 text-weight-bold">Assays</div>
-
-    <q-item>
-      <q-item-section>
-        Unfold assays and click "Add Assay" to add a new assay to your ARC
-      </q-item-section>
-    </q-item>
-
-
-    <q-tree :nodes="addAssays" node-key="label" dense default-expand-all />
-
-    <q-separator inset />
-
-    <q-item>
-      <q-item-section>
-        Click on your assay's name to open the "Assay" menu
-      </q-item-section>
-    </q-item>
-
-
-
-    <q-tree :nodes="ARCtreeAssay" node-key="label" dense selected-color='white' v-model:selected="selectedAssay"
-      default-expand-all />
-
-
-    <q-separator inset />
-
-    <div class="text-subtitle1 text-weight-bold">Protocols</div>
-
-    <q-item>
-      <q-item-section>
-        Protocols can be added to individual studies or assays, respectively.
-      </q-item-section>
-    </q-item>
-
-    <q-tree :nodes="addProtocols" node-key="label" dense default-expand-all />
-
-    <q-separator inset />
-
-    <div class="text-subtitle1 text-weight-bold">Datasets</div>
-
-    <q-item>
-      <q-item-section>
-        Dataset files or folders can be imported to individual assays.
-      </q-item-section>
-    </q-item>
-
-    <q-tree :nodes="importDatasets" node-key="label" dense default-expand-all />
-
-  </q-list>
 </template>
