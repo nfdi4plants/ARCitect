49fab488444ee4a54c2d20ce5645f56927707cfe
Jonas Lukasczyk <jl@jluk.de>
2024-02-28 11:06:46 +0100

    git dialog ok button fix

--- packages/renderer/src/dialogs/GitDialog.vue
+++ packages/renderer/src/dialogs/GitDialog.vue
@@ -68,7 +68,7 @@ const { dialogRef, onDialogHide, onDialogOK, onDialogCancel } = useDialogPluginC
       <q-linear-progress :value='100' :indeterminate='props.state===0' color="secondary" class="q-mt-sm" track-color="grey-3" />
 
       <q-card-actions align="right" style="margin:0 1.5em 1.5em;">
-        <q-btn color="secondary" :loading='props.state===0' :disabled='props.state!==1' :label="props.ok_title" @click="onDialogOK" type='submit' class='text-weight-bold'/>
+        <q-btn color="secondary" :loading='props.state===0' :disabled='props.state===0 || (props.state===2 && props.cancel_title)' :label="props.ok_title" @click="onDialogOK" type='submit' class='text-weight-bold'/>
         <q-btn v-if='props.cancel_title' color="secondary" :disabled='props.state===0' :label="props.cancel_title" @click="onDialogCancel" type='submit' class='text-weight-bold'/>
       </q-card-actions>
     </q-card>
