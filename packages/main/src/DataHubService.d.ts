cc3d998ac3891dedb3572f8d12883e2ab3062bd3
eik-dahms <eik.dahms@gmail.com>
2024-06-13 17:14:45 +0200

    added type file for DH Service

---
+++ packages/main/src/DataHubService.d.ts
@@ -0,0 +1,19 @@
+/** GitLab App authentication with Secret */
+type AuthWithSecret = {
+    id: string;
+    secret: string;
+};
+
+/** GitLab App authentication without Secret */
+type AuthIdOnly = {
+    id: string;
+    secret?: never;
+};
+  
+/** Store credetials for different datahubs. 
+Keys are adresses for the datahub.*/
+export interface Credentials {
+    [key : string] : AuthWithSecret | AuthIdOnly ;
+}
+  
+  
