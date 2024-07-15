9fee742bb6d3e5ec058562814267e9060c4d85a6
Jonas Lukasczyk <jl@jluk.de>
2024-07-15 23:24:24 +0200

    nfdi4plants.github.io added to whitelist

--- packages/main/src/security-restrictions.ts
+++ packages/main/src/security-restrictions.ts
@@ -29,6 +29,7 @@ const ALLOWED_EXTERNAL_ORIGINS = new Set<`https://${string}`>([
   'https://git.nfdi4plants.org',
   'https://gitlab.nfdi4plants.de',
   'https://nfdi4plants.org',
+  'https://nfdi4plants.github.io',
   'https://swate-alpha.nfdi4plants.org',
   'https://support.nfdi4plants.org',
   'https://gitlab.plantmicrobe.de',
