6e0c991d16e77bd2aa3151cb2f520823a136e9d8
Dominik Brilhaus <brilhaus@nfdi4plants.org>
2024-07-22 17:09:38 +0200

    shorten help menu datahub

--- packages/renderer/src/docs/datahub.vue
+++ packages/renderer/src/docs/datahub.vue
@@ -1,121 +1,32 @@
-<script setup>
-import d_input from '../components/d_input.vue';
-import d_btn from '../components/d_btn.vue';
-import d_checkbox from '../components/d_checkbox.vue';
-</script>
-
 <template>
   <div class="q-pt-sm">
-  
+
     <h5 style="margin-bottom:0.5em;">
       <q-icon color="grey-8" name="cloud_download" />
       Download ARC
     </h5>
 
-  <div class="text-body2 text-justify" style="padding-bottom:1em;">
-      Here you can download ARCs from the <a href="https://git.nfdi4plants.org/"
-        target="_blank">DataHUB</a>.
+    <div class="text-body2 text-justify" style="padding-bottom:1em;">
+      Here you can download ARCs from the <a href="https://git.nfdi4plants.org/" target="_blank">DataHUB</a>.
     </div>
-  <q-card>
-
-    <q-card-section>
-
+    <q-card>
       <q-item>
-        <q-item-section>
-          <q-field label='ARC Identifier' dense outlined readonly-hidden>
-            <template v-slot:append>
-              <q-icon name="search" />
-            </template>
-          </q-field>
-        </q-item-section>
-        <q-item-section>
           <div>
-            Use the search field to find an ARC by the name of the ARC or its creator
-          </div>
-        </q-item-section>
-      </q-item>
-
-      <q-item>
-        <q-item-section>
-          <d_checkbox label='LFS' />
-        </q-item-section>
-        <q-item-section>
-          <div>
-            (Un)check the LFS box to (not) download <a
+            With the LFS checkbox you can select wether or not download large files. Read more about <a
               href="https://nfdi4plants.org/nfdi4plants.knowledgebase/docs/guides/arc_WorkingWithLargeDataFiles.html"
-              target="_blank">large file storage (LFS)</a> objects
+              target="_blank">large file storage (LFS)</a> in the DataPLANT knowledgebase.
           </div>
-        </q-item-section>
       </q-item>
-
+    </q-card>
+    
+    <q-separator/>
+    
+    <q-card>
       <q-item>
-        <q-item-section>
-          <q-field label='Host' dense readonly-hidden>
-            <span>git.nfdi4plants.org</span>
-            <template v-slot:append>
-              <q-icon name="arrow_drop_down" />
-            </template>
-          </q-field>
-        </q-item-section>
-        <q-item-section>
           <div>
-            Select a <a href="https://git.nfdi4plants.org/" target="_blank">DataHUB</a> host from the dropdown menu
+            If you are logged in, the list of ARCs will include public and private ARCs that you have access to.
           </div>
-        </q-item-section>
       </q-item>
-
-      <q-item>
-        <q-item-section>
-          <div>
-            <d_btn icon='refresh' />
-          </div>
-        </q-item-section>
-        <q-item-section>
-          Refresh the list of available ARCs.
-        </q-item-section>
-      </q-item>
-
-    </q-card-section>
-
-    <q-separator inset />
-
-    <q-card-section>
-
-      <q-item>
-        <q-item-section>
-          The list of available ARCs is alphabetically sorted.
-        </q-item-section>
-      </q-item>
-
-
-      <q-item>
-        <q-item-section>
-          <div>
-            <d_btn icon='search' />
-          </div>
-        </q-item-section>
-        <q-item-section>
-          <div>
-            Check out an ARC in the <a href="https://git.nfdi4plants.org/" target="_blank">DataHUB</a> (opens a new
-            browser window)
-          </div>
-        </q-item-section>
-      </q-item>
-
-      <q-item>
-        <q-item-section>
-          <div>
-            <d_btn icon='file_download' />
-          </div>
-        </q-item-section>
-        <q-item-section>
-          <div>
-            Download (pull) the selected ARC from the <a href="https://git.nfdi4plants.org/" target="_blank">DataHUB</a>
-          </div>
-        </q-item-section>
-      </q-item>
-
-    </q-card-section>
-  </q-card>
-</div>
+    </q-card>
+  </div>
 </template>
