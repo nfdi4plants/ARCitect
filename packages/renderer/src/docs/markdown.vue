c0a5728c7230a8822b25d1a61b5b26e28c8dc494
Dominik Brilhaus <brilhaus@nfdi4plants.org>
2024-07-24 11:49:24 +0200

    start adding markdown help

---
+++ packages/renderer/src/docs/markdown.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="q-pt-sm">
+
+    <h5 style="margin-bottom:0.5em;">
+      Markdown editor
+    </h5>
+
+    <div class="text-body2 text-justify" style="padding-bottom:1em;">
+      Use the markdown editor to adapt text files in your ARC using markdown. 
+    </div>
+    <!-- <q-card>
+      <q-item>
+          <div>
+            TODO: link to markdown tutorial
+          </div>
+      </q-item>
+    </q-card>
+    
+    <q-separator/>
+     -->
+    <q-card>
+      <q-card-section>
+        Paste the following example into the Editor and click on Preview to see how it displays:
+      </q-card-section>
+      <q-separator/>
+      <q-card-section>        
+        <code>
+        # This is a first level header
+        <br>
+        <br>
+        ## This is a second level header
+        </code>
+      </q-card-section>
+    </q-card>
+    
+    
+  </div>
+</template>
