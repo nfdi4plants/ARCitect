03ef67e086c16f422faec348fa4612fe226a636a
Dominik Brilhaus <brilhaus@nfdi4plants.org>
2024-07-24 09:07:56 +0200

    add PAT help text

--- packages/renderer/src/docs/git-sync.vue
+++ packages/renderer/src/docs/git-sync.vue
@@ -33,15 +33,36 @@ import d_checkbox from '../components/d_checkbox.vue';
     <q-separator />
 
     <q-card>
-      <q-item>
+      <q-card-section><b>Upload of large data files</b></q-card-section>
+      <q-card-section>      
+        For upload of large amounts of data we recommend to use a personal access token (PAT). You can add a PAT generated at the DataHUB for your ARC or your DataHUB account to your remote connection:
+        <ol>
+          <li>Click <q-icon name="add_circle" color="secondary" /> to a add remote connection</li>
+          <li>Click <q-icon name="key" color="grey-7" /> to a open the PAT menu in the DataHUB</li>
+          <ul>
+            <li>Give your PAT a <b>Token name</b> (e.g. "ARCitect data upload")</li>
+            <li>Adjust the <b>Expiration date</b> (Be aware that you might have to redo this once the PAT expires) </li>
+            <li>Select a scope "api" or "write_repository"</li>
+            <li>Copy the PAT shown under <b>Your new personal access token</b> on top of the menu</li>
+          </ul>
+          <li>Paste the token string into the field Personal Access Token (Optional)</li>
+        </ol>
+      </q-card-section>
+    </q-card>
+
+    <q-separator />
+
+    <q-card>
+      <q-card-section>
         <div>
           Check out the DataPLANT knowledgebase to learn more about <a
             href="https://nfdi4plants.org/nfdi4plants.knowledgebase/docs/guides/arc_WorkingWithLargeDataFiles.html"
-            target="_blank">large file storage (LFS)</a>.
-
+            target="_blank">large file storage (LFS)</a> or <a
+            href="https://nfdi4plants.org/nfdi4plants.knowledgebase/docs/guides/datahub-GeneratePAT.html"
+            target="_blank">generating personal access tokens</a>.
             <!-- TODO: add explanation about remotes  -->
         </div>
-      </q-item>
+      </q-card-section>
     </q-card>
 
 </div></template>
