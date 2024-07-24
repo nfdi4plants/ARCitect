ff76b48990f372d4eb5a792f3b281af39df221d5
Dominik Brilhaus <brilhaus@nfdi4plants.org>
2024-07-24 14:27:24 +0200

    slim main help

--- packages/renderer/src/docs/home.vue
+++ packages/renderer/src/docs/home.vue
@@ -1,11 +1,6 @@
-<!-- 
-TODO: fix logo import 
--->
-
 <script lang="ts">
 import { ref } from 'vue'
 
-import logoURL from '/assets/dpLogo2_w.png';
 
 export default {
   setup() {
@@ -91,225 +86,50 @@ export default {
 <template>
   <div class="q-pa-sm">
 
-    <h5 style="margin-bottom:0.5em;">Help</h5>
+    <h5 style="margin-bottom:0.5em;">Help menu</h5>
 
     <q-card>
+      <q-card-section style="font-size: 1.2em;font-weight: bold;">
+        Overview
+      </q-card-section>
+      <q-card-section>
+      The ARCitect is divided into multiple panels (left to right):
+      <q-list class='bg-grey-3' dense style="border-radius:0.3em; margin:0.2em;">
+      <q-item>Sidebar</q-item>
+      <q-item>File Tree</q-item>
+      <q-item>Main panel</q-item>
+      <q-item>This help menu (if activated)</q-item>
+    </q-list>
+    </q-card-section>
+
+    <q-separator inset style="height:0.2em"/>
+
       <q-card-section>
         Click <q-icon color="grey-7" name="help" />Toggle Help in the sidebar to show or hide the help menu.
       </q-card-section>
-      <q-separator/>
+    
+      <q-separator inset style="height:0.2em"/>
+    
       <q-card-section>
-        Click <q-icon color="grey-7" name="sym_r_tooltip" />Toggle Tooltips in the sidebar to show or hide hover-over help texts at various functions.
+        Click <q-icon color="grey-7" name="sym_r_mark_chat_read"/> / <q-icon color="grey-7" name="sym_r_chat_bubble"/> Toggle Tooltips in the sidebar to show or hide hover-over help texts at various functions.
       </q-card-section>
     </q-card>
 
-    <div class="q-pa-sm text-body2 text-justify">
-      The ARCitect is divided into three panels: sidebar, file tree and main menu.
-    </div>
+    <q-separator inset style="height:0.2em"/>
 
-    <q-tabs v-model="tab" dense class="text-grey" active-color="primary" indicator-color="primary" align="justify"
-      narrow-indicator>
-      <q-tab name="sidebar" label="Sidebar" />
-      <q-tab name="filetree" label="File Tree" />
-    </q-tabs>
-
-    <q-tab-panels v-model="tab" animated>
-      <q-tab-panel name="sidebar">
-
-        <div class="q-pa-sm text-body2 text-justify">
-          Navigate the ARCitect via the sidebar on the left.
-        </div>
-
-        <q-list class="text-justify bg-grey-2">
-          <q-item>
-
-            <q-item class='bg-primary text-white' style="padding-top:1em;padding-bottom:1em;">
-              <q-item-section avatar>
-                <q-icon size="2.5rem" style="margin: 0 -0.20em;" :name="'img:/assets/dpLogo2_w.png'"></q-icon>
-                <!-- <q-icon size="2.5rem" style="margin: 0 -0.20em;" :name="'img:' + logoURL"></q-icon> -->
-              </q-item-section>
-              <q-item-section style="margin:0.6em 0 0 -1.2em">
-                <q-item-label><b style="font-size:2em">ARC</b><span style="font-size:1.2em">itect</span></q-item-label>
-              </q-item-section>
-            </q-item>
-
-            <q-separator inset />
-
-            <q-item-section>
-              Return to home view
-            </q-item-section>
-          </q-item>
-
-          <q-separator inset />
-
-          <q-item>
-            <q-item-section avatar>
-              <q-icon color="grey-7" name="exit_to_app" />
-            </q-item-section>
-            <q-item-section style="margin-left: -1.2em;">Login</q-item-section>
-            <q-item-section>
-              Login to the <a href="https://git.nfdi4plants.org/" target="_blank">DataHUB</a>
-            </q-item-section>
-          </q-item>
-
-          <q-item>
-            <q-item-section avatar>
-              <q-icon color="grey-7" name="note_add" />
-            </q-item-section>
-            <q-item-section style="margin-left: -1.2em;">
-              New ARC
-            </q-item-section>
-            <q-item-section>
-              Create a new ARC on your computer
-            </q-item-section>
-          </q-item>
-
-          <q-item>
-            <q-item-section avatar>
-              <q-icon color="grey-7" name="file_open" />
-            </q-item-section>
-            <q-item-section style="margin-left: -1.2em;">
-              Open ARC
-            </q-item-section>
-            <q-item-section>
-              Open an ARC existing from your computer
-            </q-item-section>
-          </q-item>
-
-          <q-item>
-            <q-item-section avatar>
-              <q-icon color="grey-7" name="cloud_download" />
-            </q-item-section>
-            <q-item-section style="margin-left: -1.2em;">
-              Download ARC
-            </q-item-section>
-            <q-item-section>
-              Download an ARC from the <a href="https://git.nfdi4plants.org/" target="_blank">DataHUB</a>
-            </q-item-section>
-          </q-item>
-
-          <q-separator inset />
-
-          <q-item>
-            <q-item-section avatar>
-              <q-icon color="grey-7" name="save" />
-            </q-item-section>
-            <q-item-section style="margin-left: -1.2em;">
-              Save ARC
-            </q-item-section>
-            <q-item-section>
-              Save the ARC locally
-            </q-item-section>
-          </q-item>
-          
-          <q-item>
-            <q-item-section avatar>
-              <q-icon color="grey-7" name="folder_open" />
-            </q-item-section>
-            <q-item-section style="margin-left: -1.2em;">
-              Explorer
-            </q-item-section>
-            <q-item-section>
-              Open the ARC in your Explorer or Finder on your computer
-            </q-item-section>
-          </q-item>
-
-          <q-separator inset />
-
-          <q-item>
-            <q-item-section avatar>
-              <q-icon color="grey-7" name="rule" />
-            </q-item-section>
-            <q-item-section style="margin-left: -1.2em;">
-              Commit
-            </q-item-section>
-            <q-item-section>
-              Track and communicate changes of your ARC
-            </q-item-section>
-          </q-item>
-
-          <q-item>
-            <q-item-section avatar>
-              <q-icon color="grey-7" name="published_with_changes" />
-            </q-item-section>
-            <q-item-section style="margin-left: -1.2em;">
-              DataHUB Sync
-            </q-item-section>
-            <q-item-section>
-              Synchronize the ARC with a DataHUB
-            </q-item-section>
-          </q-item>
-          
-          <q-item>
-            <q-item-section avatar>
-              <q-icon color="grey-7" name="history" />
-            </q-item-section>
-            <q-item-section style="margin-left: -1.2em;">
-              History
-            </q-item-section>
-            <q-item-section>
-              Inspect the ARC's history
-            </q-item-section>
-          </q-item>
-
-          <q-separator inset />
-                    
-          <q-item>
-            <q-item-section avatar>
-              <q-icon color="grey-7" name="verified" />
-            </q-item-section>
-            <q-item-section style="margin-left: -1.2em;">
-              Validation
-            </q-item-section>
-            <q-item-section>
-              Choose ARC validation
-            </q-item-section>
-          </q-item>
-
-          <q-separator inset />
-
-          <q-item>
-            <q-item-section avatar>
-              <q-icon color="grey-7" name="help" />
-            </q-item-section>
-            <q-item-section style="margin-left: -1.2em;">
-              Toggle Help
-            </q-item-section>
-            <q-item-section>
-              Show or hide the help menu
-            </q-item-section>
-          </q-item>
-
-          <q-item>
-            <q-item-section avatar>
-              <q-icon color="grey-7" name="chevron_left" />
-            </q-item-section>
-            <q-item-section style="margin-left: -1.2em;">
-              Minimize Sidebar
-            </q-item-section>
-            <q-item-section>
-              Minimize or maximize the sidebar
-            </q-item-section>
-          </q-item>
-
-        </q-list>
-
-
-      </q-tab-panel>
-
-      <q-tab-panel name="filetree">
-
-        <div class="q-pa-sm text-body2 text-justify">
-          Access and edit the files in your ARC via the file tree.
-        </div>
+    <q-card>
+      <q-card-section style="font-size: 1.2em;font-weight: bold;">
+        File Tree (middle panel)
+      </q-card-section>
+      <q-card-section>
+      You can access and edit the files in your ARC via the file tree.
 
         <q-list class="q-pa-md q-gutter-sm">
 
-          <q-item>
             <q-item-section>
               The root of your ARC by default contains the folders: studies - assays - workflows - runs
             </q-item-section>
-          </q-item>
+
 
           <q-tree :nodes="ARCtreeInv" node-key="label" dense default-expand-all />
 
@@ -324,7 +144,7 @@ export default {
           </q-item>
 
 
-          <q-tree :nodes="ARCtreeInv" node-key="label" dense selected-color='white' v-model:selected="selectedInv"
+          <q-tree :nodes="ARCtreeInv" node-key="label" dense selected-color='grey-10' v-model:selected="selectedInv"
             default-expand-all />
 
           <q-separator inset />
@@ -347,7 +167,7 @@ export default {
             </q-item-section>
           </q-item>
 
-          <q-tree :nodes="ARCtreeStudy" node-key="label" dense selected-color='white' v-model:selected="selectedStudy"
+          <q-tree :nodes="ARCtreeStudy" node-key="label" dense selected-color='grey-10' v-model:selected="selectedStudy"
             default-expand-all />
 
           <q-separator inset />
@@ -373,7 +193,7 @@ export default {
 
 
 
-          <q-tree :nodes="ARCtreeAssay" node-key="label" dense selected-color='white' v-model:selected="selectedAssay"
+          <q-tree :nodes="ARCtreeAssay" node-key="label" dense selected-color='grey-10' v-model:selected="selectedAssay"
             default-expand-all />
 
 
@@ -402,7 +222,10 @@ export default {
           <q-tree :nodes="importDatasets" node-key="label" dense default-expand-all />
 
         </q-list>
-      </q-tab-panel>
-    </q-tab-panels>
-  </div>
+      </q-card-section>
+      </q-card>
+    
+    
+    </div>
+
 </template>
