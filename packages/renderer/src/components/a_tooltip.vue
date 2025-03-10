9d8fcc706759a0e798eec0a647d48a60ff5fa5c3
Jonas Lukasczyk <mail@jluk.de>
2025-03-10 14:08:55 +0100

    improved date rendering in DataHub view

--- packages/renderer/src/components/a_tooltip.vue
+++ packages/renderer/src/components/a_tooltip.vue
@@ -1,9 +1,15 @@
 <script lang="ts" setup>
 import AppProperties from '../AppProperties.ts';
+
+defineProps<{
+  force?: Boolean
+}>()
+
+
 </script>
 
 <template>
-  <q-tooltip v-if='AppProperties.config.showTooltips' style="font-size:1.1em">
+  <q-tooltip v-if='AppProperties.config.showTooltips || force' style="font-size:1.1em">
     <slot></slot>
   </q-tooltip>
 </template>
