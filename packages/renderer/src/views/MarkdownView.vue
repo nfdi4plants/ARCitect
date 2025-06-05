180598cf3839babb22f8b2055e9862b0706ae6b4
Jonas Lukasczyk <jl@jluk.de>
2025-06-05 15:47:27 +0200

    better markdown/text editor

--- packages/renderer/src/views/MarkdownView.vue
+++ packages/renderer/src/views/MarkdownView.vue
@@ -1,148 +1,208 @@
 <script lang="ts" setup>
 
-import { reactive, watch, onMounted, onUnmounted} from 'vue';
+import { reactive, watch, onMounted, onUnmounted, ref} from 'vue';
 
 import Markdown from 'vue3-markdown-it';
 import ViewItem from '../components/ViewItem.vue';
 import a_input from '../components/a_input.vue';
+import ConfirmationDialog from '../dialogs/ConfirmationDialog.vue';
 import AppProperties from '../AppProperties.ts';
 import ArcControlService from '../ArcControlService.ts';
 
+import { VMarkdownEditor } from 'vue3-markdown';
+import 'vue3-markdown/dist/vue3-markdown.css';
+
+import { useQuasar } from 'quasar'
+const $q = useQuasar();
+
+const editor = ref();
+let observer = null;
 const iProps = reactive({
   path: '',
   text: '',
-  original: '',
-  processed: '',
+  initialized: false,
   base64Images: new Map(),
-  tab: 'editor'
 });
 
-const extractFilePaths = markdown => {
-  const regex = /!\[.*?\]\((.*?)\s*(?:"[^"]*")?\)/g;
-  const filePaths = [];
-  let match;
-  while ((match = regex.exec(markdown)) !== null)
-    filePaths.push(match[1]);
-  return filePaths;
+const insertText = text=>{
+  const textarea = editor.value.editor;
+  const position = textarea.selectionStart;
+  const before = textarea.value.substring(0, position);
+  const after = textarea.value.substring(position, textarea.value.length);
+  textarea.value = before + text + after;
+  textarea.selectionStart = textarea.selectionEnd = position + text.length;
+  iProps.text = textarea.value;
 };
 
-const processText = async ()=>{
-  console.log('edit');
+const addImages = async ()=>{
+  const images = await window.ipc.invoke('LocalFileSystemService.selectAnyFiles');
 
-  let text = iProps.text;
-  for(let path of extractFilePaths(text)){
-    let value = null;
-    if(iProps.base64Images.has(path)){
-      value = iProps.base64Images.get(path);
-    } else {
-      if(path.startsWith('http://') || path.startsWith('https://')){
-        iProps.base64Images.set(path,path);
-      } else {
-        const imageAs64 = await window.ipc.invoke('LocalFileSystemService.readImage', ArcControlService.props.arc_root+'/'+path);
-        iProps.base64Images.set(path,imageAs64);
+  if(images.filter(x=>!x.startsWith(ArcControlService.props.arc_root)).length)
+    return $q.dialog({
+      component: ConfirmationDialog,
+      componentProps: {
+          title: 'Error',
+          msg: 'Images bust be located inside ARC'
       }
+    });
+
+  for(let path of images){
+    const relPath = getRelativePath(iProps.path,path);
+    console.log(relPath);
+    insertText(`![${relPath.split('/').pop().split('.')[0]}](${relPath})`);
+  }
+};
+
+const getRelativePath = (a, b)=> {
+
+  const fromParts = a.split('/');
+  const toParts = b.split('/');
+  fromParts.pop();
+  const filename = toParts.pop();
+  const length = Math.min(fromParts.length, toParts.length);
+  let samePartsLength = length;
+  for(let i = 0; i < length; i++)
+    if(fromParts[i] !== toParts[i]){
+      samePartsLength = i;
+      break;
+    }
+
+  let outputParts = [];
+  for (let i = samePartsLength; i < fromParts.length; i++)
+    outputParts.push('..');
+
+  outputParts = outputParts.concat(toParts.slice(samePartsLength));
+  return outputParts.concat([filename]).join('/');
+};
+
+const replaceLocalImageWithBase64 = async img => {
+  if(!img.src.includes('localhost:'))
+    return;
+  const src = ArcControlService.props.arc_root+'/'+(new URL(img.src)).pathname;
+  let base64Image = iProps.base64Images.get(src);
+  if(!base64Image){
+    base64Image = await window.ipc.invoke('LocalFileSystemService.readImage', src);
+    iProps.base64Images.set(src,base64Image);
+  }
+  img.src = base64Image;
+};
+
+const replaceAllLocalImagesWithBase64 = async () => {
+  const container = editor.value.box.getElementsByClassName('vmd-view')[0];
+  const imgs = container.getElementsByTagName('img');
+  for(let img of imgs)
+    replaceLocalImageWithBase64(img);
+};
+
+const handleNewImages = (mutationsList) => {
+  for (const mutation of mutationsList) {
+    if (mutation.type === 'childList') {
+      mutation.addedNodes.forEach(node => {
+        if (node.nodeType === 1) { // Element node
+          if (node.tagName === 'IMG') {
+            replaceLocalImageWithBase64(node)
+          } else if (node.querySelectorAll) {
+            const imgs = node.querySelectorAll('img');
+            imgs.forEach(async img => {
+              replaceLocalImageWithBase64(img);
+            });
+          }
+        }
+      });
     }
   }
-  for(let [path,path_] of iProps.base64Images)
-    text = text.replaceAll(path,path_);
-  iProps.processed = text;
+};
+
+const createButton = (icon,callback)=>{
+  const iconElement = document.createElement('i');
+  iconElement.classList.add('q-icon', 'notranslate', 'material-icons', 'q-item--clickable');
+  iconElement.setAttribute('aria-hidden', 'true');
+  iconElement.style['font-size'] = '1.4em';
+  iconElement.style.cursor = 'pointer';
+  iconElement.style.color = '#777';
+  iconElement.textContent = icon;
+  iconElement.onclick = callback;
+
+  const button = document.createElement('div');
+  button.classList.add('vmd-trigger');
+  button.appendChild(iconElement);
+
+  return button;
 };
 
 const init = async ()=>{
   iProps.path = AppProperties.active_markdown;
-  iProps.text = '';
-  iProps.original = '';
   iProps.text = await window.ipc.invoke('LocalFileSystemService.readFile', iProps.path);
-  iProps.original = iProps.text;
-  processText();
+
+  replaceAllLocalImagesWithBase64();
+
+  if(iProps.initialized) return;
+
+  // add custom image upload button
+  const menu = editor.value.box.getElementsByClassName('vmd-toolbar')[0];
+  const imageButton = menu.children[28];
+  const newImageButton = createButton('image',addImages);
+  menu.insertBefore(newImageButton,imageButton);
+  menu.removeChild(imageButton);
+
+  menu.prepend(createButton('refresh',init));
+  menu.prepend(createButton('save',save));
+
+  // add image listener
+  const container = editor.value.box.getElementsByClassName('vmd-view')[0];
+  observer = new MutationObserver(handleNewImages);
+  observer.observe(container, {
+    childList: true,
+    subtree: true,
+  });
+
+  iProps.initialized = true;
 };
 
 const save = async ()=>{
-  if(iProps.text===iProps.original) return;
   await window.ipc.invoke('LocalFileSystemService.writeFile', [iProps.path,iProps.text]);
   init();
 };
 
 onMounted(init);
-onUnmounted(save);
+onUnmounted(()=>{
+  observer.disconnect();
+});
 watch( ()=>AppProperties.active_markdown, init );
-watch( ()=>iProps.tab, processText );
 
 </script>
 
 <template>
-  <q-list>
-    <ViewItem
-      icon="edit_note"
-      label="Markdown Editor"
-      :caption="iProps.path"
-    >
-      <q-card flat>
-        <q-tabs
-          v-if='iProps.path.endsWith(".md")'
-          v-model="iProps.tab"
-          dense
-          class="text-grey"
-          active-color="primary"
-          indicator-color="primary"
-          align="justify"
-          narrow-indicator
-        >
-          <q-tab name="editor" label="Editor" />
-          <q-tab name="preview" label="Preview" />
-        </q-tabs>
+  <div class='fit markdown_container'>
+    <div>
+      <ViewItem
+        icon="edit_note"
+        label="Markdown Editor"
+        :caption="iProps.path"
+      >
+      </ViewItem>
+    </div>
 
-        <q-separator />
-
-        <q-tab-panels v-model="iProps.tab" class='q-pa-none'>
-          <q-tab-panel name="editor" class='q-pl-none q-pr-none'>
-
-            <a_input v-model='iProps.text' type='textarea' autogrow :bg-color='iProps.text!==iProps.original ? "teal-1":""' />
-            <q-card-actions align='right' style="padding:0.5em 1em;">
-              <q-btn label="Save" type="submit" icon='check_circle' color="secondary" @click='save'/>
-              <q-btn label="Reset" type="submit" icon='refresh' color="secondary" @click='init'/>
-            </q-card-actions>
-          </q-tab-panel>
-
-          <q-tab-panel name="preview">
-            <Markdown class='markdown' :source="iProps.processed" />
-          </q-tab-panel>
-        </q-tab-panels>
-      </q-card>
-    </ViewItem>
-  </q-list>
+    <div class='markdown_container_div'>
+      <VMarkdownEditor
+        ref='editor'
+        v-model="iProps.text"
+        locale="en"
+      />
+    </div>
+    <div>&nbsp;</div>
+  </div>
 </template>
 
 <style>
-
-  .toppad .q-field__control-container {
-    padding-top:0.6em !important;
+  .markdown_container {
+    display: block;
+    display:flex;
+    flex-direction: column;
   }
 
-  .markdown * {
-    margin: 0.2em 0em;
-    line-height: 1.2;
+  .markdown_container_div {
+    flex-grow:1;
+    overflow: hidden;
   }
-  .markdown h1 {
-    font-size:3em;
-    margin-top: 1em;
-  }
-  .markdown h2 {
-    font-size:2em;
-    margin-top: 1em;
-  }
-  .markdown h3 {
-    font-size:2em;
-    margin-top: 1em;
-  }
-  /*.markdown h4 {*/
-  /*  font-size:1.4em;*/
-  /*}*/
-  /*.markdown h5 {*/
-  /*  font-size:1.2em;*/
-  /*}*/
-  /*.markdown h6 {*/
-  /*  font-size:1em;*/
-  /*}*/
-
 </style>
