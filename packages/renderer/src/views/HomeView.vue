a1c9197f3eb9f546801b866b52e9ca737a177756
Kevin F <Freymaurer@gmx.de>
2025-09-10 10:13:30 +0200

    Rmv confetti player, update vite dependencies, release with new Swate #409

--- packages/renderer/src/views/HomeView.vue
+++ packages/renderer/src/views/HomeView.vue
@@ -2,7 +2,6 @@
 
 import {onMounted,onUnmounted} from 'vue';
 import logoURL from '../../assets/nfdi-hero.svg';
-import confetti from 'canvas-confetti';
 
 onMounted(()=>{
   var duration = 10 * 1000;
@@ -17,22 +16,6 @@ onMounted(()=>{
     var timeLeft = animationEnd - Date.now();
     var ticks = Math.max(200, 500 * (timeLeft / duration));
     skew = Math.max(0.8, skew - 0.001);
-
-    confetti({
-      particleCount: 1,
-      startVelocity: 0,
-      ticks: ticks,
-      origin: {
-        x: Math.random(),
-        // since particles fall down, skew start toward the top
-        y: (Math.random() * skew) - 0.2
-      },
-      colors: ['#0bb5dd','#2d3e50'],
-      shapes: ['circle', 'square'],
-      gravity: randomInRange(0.4, 0.6),
-      scalar: randomInRange(0.4, 1),
-      drift: randomInRange(-0.4, 0.4)
-    });
 
     if (timeLeft > 0) {
       requestAnimationFrame(frame);
