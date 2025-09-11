a1240440f63dd872cf45d4e0d391c5fe2b63971e
Dominik Brilhaus <brilhaus@nfdi4plants.org>
2025-09-11 10:15:55 +0200

    remove version from home view

--- packages/renderer/src/views/HomeView.vue
+++ packages/renderer/src/views/HomeView.vue
@@ -29,7 +29,7 @@ onMounted(()=>{
 
   <div style="text-align:center;">
     <img :src="logoURL" style="box-sizing:border-box;width:100%;padding:4em;max-width:900px;"/>
-    <div class='text-h4'><span style="border-bottom:0.1em solid #000;">Welcome to <b>ARC</b>itect <b>v1.0.0</b>!</span></div>
+    <div class='text-h4'><span style="border-bottom:0.1em solid #000;">Welcome to <b>ARC</b>itect!</span></div>
     <div class='text-h6' style="line-height:1em;padding-top:0.5em;">Democratization of plant research<br>made easy.</div>
   </div>
 
