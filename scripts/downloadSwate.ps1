da7d0ebf6181ffe7301ff16c71b073b4a4f9c88c
Kevin F <Freymaurer@gmx.de>
2025-11-20 20:43:21 +0100

    update swate version

--- scripts/downloadSwate.ps1
+++ scripts/downloadSwate.ps1
@@ -1,7 +1,7 @@
 # Variables
 $Owner = "nfdi4plants"
 $Repo = "Swate"
-$TagName = "1.0.5"
+$TagName = "1.0.6"
 $AssetName = "SwateClient.zip"
 $DownloadUrl = "https://github.com/$Owner/$Repo/releases/download/$TagName/$AssetName"
 $DestDir = "./resources/swate"
